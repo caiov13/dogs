5c36ad18b4801cefedb030a94a031fdeac76e492
Caio Vinicius <caiov3d@outlook.com>
2023-05-18 21:55:34 -0300

    aula sobre memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+
 function App() {
+  const [contar, setContar] = React.useState(0);
   return (
     <div>
       <h1>Hello</h1>
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
     </div>
   );
 }
