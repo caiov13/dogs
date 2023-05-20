75ff390a1254448887ef48dbf38e7fd5fee5a497
Caio Vinicius <caiov3d@outlook.com>
2023-05-19 22:58:19 -0300

    use reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+function reducer(state, action) {
+  switch (action) {
+    case 'aumentar':
+      return state + 1;
+    case 'diminuir':
+      return state - 1;
+    default:
+      throw new Error();
+  }
+}
+
 function App() {
-  const [contar, setContar] = React.useState(0);
+  const [state, dispatch] = React.useReducer(reducer, 0);
   return (
     <div>
-      <h1>Hello</h1>
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+      <h1>{state}</h1>
+      <button onClick={() => dispatch('aumentar')}>+</button>
+      <button onClick={() => dispatch('diminuir')}>-</button>
     </div>
   );
 }
