3a0a30ee630e1b73f71517975f9504e9583d0769
Caio Vinicius <caiov3d@outlook.com>
2023-05-23 20:57:48 -0300

    configuracoes iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import React from 'react';
-
-function reducer(state, action) {
-  switch (action) {
-    case 'aumentar':
-      return state + 2;
-    case 'diminuir':
-      return state - 1;
-    default:
-      throw new Error();
-  }
-}
-
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, 0);
   return (
     <div>
-      <h1>{state}</h1>
-      <button onClick={() => dispatch('aumentar')}>+</button>
-      <button onClick={() => dispatch('diminuir')}>-</button>
+      <h1>Olá</h1>
     </div>
   );
 }
