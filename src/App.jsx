ad2496bc915d3c96f69b60db89a002c40ad4f5cf
Caio Vinicius <caiov3d@outlook.com>
2023-05-24 22:03:36 -0300

    estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
 function App() {
   return (
     <div>
-      <h1>Olá</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
