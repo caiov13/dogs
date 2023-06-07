9068b8b0ac9eda5267c37a0a5688ab1548b6c243
Caio Vinicius <caiov3d@outlook.com>
2023-06-06 22:40:04 -0300

    postar foto

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -9,6 +9,10 @@ const types = {
     regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
     message: 'A senha precisa ter 1 caracter maísculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracters',
   },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize números apenas.',
+  },
 };
 
 function useForm(type) {
