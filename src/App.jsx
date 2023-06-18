49b1ef555832c84a86470506f0977e5976eb32b5
Caio Vinicius <caiov3d@outlook.com>
2023-06-18 19:57:48 -0300

    pagina de erro

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import User from './components/User/User';
 import ProtectedRoute from './Helper/ProtectedRoute';
 import Photo from './components/Photo/Photo';
 import UserProfile from './components/User/UserProfile';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
             />
             <Route path="foto/:id" element={<Photo />} />
             <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
