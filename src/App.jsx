eaf967b8fbf8d96cd66bf63834bc52498d8dfcba
Caio Vinicius <caiov3d@outlook.com>
2023-06-18 19:51:03 -0300

    pagina de perfil do usuario

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { UserStorage } from './UserContext';
 import User from './components/User/User';
 import ProtectedRoute from './Helper/ProtectedRoute';
 import Photo from './components/Photo/Photo';
+import UserProfile from './components/User/UserProfile';
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
